1a110cc415a6439b7de5a05b068b53719bc975c3
Antony Thompson <tonyt@Antonys-MacBook-Pro.local>
2019-07-28 19:15:21 +0100

    GH-11 change button descriptors  as PR note

--- bananagram-frontend/src/components/Grid/GridControls/GridControls.tsx
+++ bananagram-frontend/src/components/Grid/GridControls/GridControls.tsx
@@ -46,16 +46,13 @@ export const GridControls: FC<GridControlsProps> = ({
     <ControlButtonsWrapper>
       <Button onClick={onZoomOut}>-</Button>
       <Button onClick={onSave}>
-        Save{'('}
-        {`${offset.x}, ${offset.y}@${dimensions.x}x${dimensions.y})`}
+        {`Save(${offset.x}, ${offset.y}@${dimensions.x}x${dimensions.y})`}
       </Button>
       <Button onClick={onHome}>
-        Home{'('}
-        {`${initialOffset.x}, ${initialOffset.y}@${initialGridDimensions.x}x${initialGridDimensions.y})`}
+        {`Home(${initialOffset.x}, ${initialOffset.y}@${initialGridDimensions.x}x${initialGridDimensions.y})`}
       </Button>
       <Button onClick={onRestore}>
-        Restore{'('}
-        {`${offsetSaved.x}, ${offsetSaved.y}@${dimensionsSaved.x}x${dimensionsSaved.y})`}
+        {`Restore(${offsetSaved.x}, ${offsetSaved.y}@${dimensionsSaved.x}x${dimensionsSaved.y})`}
       </Button>
       <Button onClick={onZoomIn}>+</Button>
     </ControlButtonsWrapper>
