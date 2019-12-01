b1d854cfb0d554903dc135126109d2658dbb3501
tonyt50 <tony_t50@hotmail.com>
2019-12-01 17:57:56 +0000

    GH-52 remove example code, used for testing ideas.

--- packages/bananagram-common/src/example.test.ts
+++
@@ -1,6 +0,0 @@
-import { createPieces, numberOfBanangramTokens } from './example';
-
-it('should do something', () => {
-  const pieces = createPieces(numberOfBanangramTokens);
-  console.log('Examples Pieces:', JSON.stringify(pieces));
-});
