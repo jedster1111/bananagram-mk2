6d3c651aa1c3655ee4a36d4cba30f8d74864ebb8
tonyt50 <tony_t50@hotmail.com>
2019-12-01 14:09:13 +0000

    GH-52 Set up GitHub actions testing

--- packages/bananagram-common/jest.config.js
+++ packages/bananagram-common/jest.config.js
@@ -3,15 +3,6 @@ module.exports = {
     {
       preset: 'ts-jest',
       testEnvironment: 'node',
-      displayName: 'bananna-common-test',
-    },
-    {
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      displayName: 'banana-common-lint',
-      runner: 'jest-runner-eslint',
-      testMatch: ['<rootDir>/src/**/*.ts'],
     },
   ],
-  watchPlugins: ['jest-runner-eslint/watch-fix'],
 };
